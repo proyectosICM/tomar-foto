e7bf95b0df0865484ab42a567264fc1bbca15f38
Eduardo Aguilar <es123y123@gmail.com>
2023-08-04 15:47:47 -0500

    Se suben las imagenes

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
+// Código del Grupo 1
 import React, { useState, useEffect } from 'react';
 import { View, Image, StyleSheet, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { Button } from 'react-native-elements';
 import axios from 'axios';
-import * as FileSystem from 'expo-file-system';
-import * as MediaLibrary from 'expo-media-library'; // Importamos la librería
 
 const App = () => {
   const [image, setImage] = useState(null);
@@ -17,44 +16,6 @@ const App = () => {
       }
     })();
   }, []);
-
-  const uploadImageToImgur = async (imageUri) => {
-    try {
-      const base64Image = await convertImageToBase64(imageUri);
-
-      const response = await axios.post(
-        'https://api.imgur.com/3/image',
-        { image: base64Image },
-        {
-          headers: {
-            'Authorization': 'Client-ID ee0c72edd17d07a',
-          },
-        }
-      );
-
-      if (response.status === 200) {
-        // La imagen se ha subido exitosamente.
-        const uploadedImageUrl = response.data.data.link;
-        console.log('Imagen subida con éxito:', uploadedImageUrl);
-        // Aquí puedes hacer lo que desees con la URL de la imagen subida (por ejemplo, guardarla en tu base de datos, mostrarla en tu aplicación, etc.).
-      } else {
-        console.log('Error al subir la imagen:', response.data);
-      }
-    } catch (error) {
-      console.log('Error al subir la imagen:', error);
-    }
-  };
-
-  const convertImageToBase64 = async (imageUri) => {
-    try {
-      const base64Image = await FileSystem.readAsStringAsync(imageUri, {
-        encoding: FileSystem.EncodingType.Base64,
-      });
-      return `data:image/jpeg;base64,${base64Image}`;
-    } catch (error) {
-      throw new Error('Error al convertir la imagen a base64: ' + error);
-    }
-  };
 
   const handleImagePicker = async () => {
     try {
@@ -72,23 +33,29 @@ const App = () => {
     }
   };
 
-  const handleEnviar = () => {
-    if (image) {
-      // Subir la imagen al servidor antes de establecerla en null.
-      uploadImageToImgur(image);
-    }
-    //setImage(null);
-  };
-
-  const handleGuardar = async () => {
+  const handleEnviar = async () => {
     if (image) {
       try {
-        const asset = await MediaLibrary.createAssetAsync(image); // Guardar la imagen en la galería
-        if (asset) {
-          Alert.alert('Imagen guardada', 'La imagen se ha guardado en la galería.');
+        const formData = new FormData();
+        formData.append('image', { uri: image, name: 'image.jpg', type: 'image/jpeg' });
+        
+        const response = await axios.post('https://api.imgur.com/3/image', formData, {
+          headers: {
+            'Authorization': 'Client-ID ee0c72edd17d07a',
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+
+        if (response.status === 200) {
+          // La imagen se ha subido exitosamente.
+          const uploadedImageUrl = response.data.data.link;
+          console.log('Imagen subida con éxito:', uploadedImageUrl);
+          setImage(null)
+        } else {
+          console.log('Error al subir la imagen:', response.data);
         }
       } catch (error) {
-        Alert.alert('Error', 'Hubo un error al guardar la imagen: ' + error);
+        console.log('Error al subir la imagen:', error);
       }
     }
   };
@@ -98,10 +65,7 @@ const App = () => {
       {image ? (
         <>
           <Image source={{ uri: image }} style={styles.image} resizeMode="contain" />
-          <View style={styles.buttonsContainer}>
-            <Button title={'Ed'} onPress={handleEnviar} />
-            <Button title={'Guardar'} onPress={handleGuardar} />
-          </View>
+          <Button title={'Enviar'} onPress={handleEnviar} />
         </>
       ) : (
         <View style={styles.previewContainer}>
@@ -130,10 +94,6 @@ const styles = StyleSheet.create({
   image: {
     width: '50%',
     height: '50%',
-  },
-  buttonsContainer: {
-    flexDirection: 'row',
-    marginTop: 20,
   },
 });
 
